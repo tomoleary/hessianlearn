ba3537fc46cf3aa842a6e461dc860cb34f236cd1
Tom O'Leary-Roseberry <tom.olearyroseberry@utexas.edu>
2019-05-09 19:47:31 -0500

    indentation

--- applications/mnist/mnist_run.py
+++ applications/mnist/mnist_run.py
@@ -13,15 +13,15 @@ import pickle
 from argparse import ArgumentParser
 
 parser = ArgumentParser(add_help=True, description="-b batch size (int) \
-                                                        -p population_size (int) -alpha (float)")
+														-p population_size (int) -alpha (float)")
 # parser.add_argument('-hl',dest = 'path_to_hl',required=True, help="path to hippylearn, required!",type=str)
 parser.add_argument("-optimizer", dest='optimizer',required=False, default = 'incg', help="optimizer type",type=str)
 parser.add_argument('-alpha',dest = 'alpha',required = False,default = 1e-3,help= 'learning rate alpha',type=float)
 parser.add_argument('-sfn_lr',dest = 'sfn_lr',required= False,default = 40,help='low rank for sfn',type = int)
 parser.add_argument('-record_spectrum',dest = 'record_spectrum',\
-                    required= False,default = 0,help='boolean for recording spectrum',type = int)
+					required= False,default = 0,help='boolean for recording spectrum',type = int)
 parser.add_argument('-weight_burn_in',dest = 'weight_burn_in',\
-                    required= False,default = 0,help='',type = int)
+					required= False,default = 0,help='',type = int)
 parser.add_argument('-n_threads',dest = 'n_threads',required= False,default = 2,help='threads',type = int)
 parser.add_argument('-batch_ratio',dest = 'batch_ratio',required= False,default = 0.1,help='threads',type = float)
 
@@ -49,7 +49,7 @@ data_func = load_mnist
 raw_data = data_func()
 
 data = Data(raw_data,training_data_size,\
-        batch_size,hessian_batch_size = hess_batch_size,test_data_size = testing_data_size)
+		batch_size,hessian_batch_size = hess_batch_size,test_data_size = testing_data_size)
 
 # Define network and instantiate problem and regularization
 architecture = {}
@@ -94,7 +94,7 @@ sess.run(problem._assign_to_w(w_0))
 if args.optimizer == 'adam':
 	batch_size = int(args.batch_ratio*training_data_size)
 	data = Data(raw_data,training_data_size,\
-        batch_size,hessian_batch_size = hess_batch_size,test_data_size = testing_data_size)
+		batch_size,hessian_batch_size = hess_batch_size,test_data_size = testing_data_size)
 	print(('Using Adam optimizer, with '+str(100*args.batch_ratio)+'% mini-batches').center(80))
 	print(('Batch size = '+str(batch_size)).center(80))
 	optimizer = Adam(problem,regularization,sess)
@@ -121,7 +121,7 @@ elif args.optimizer == 'lrsfn':
 	print('Using low rank SFN optimizer with line search'.center(80))
 	print(('Batch size = '+str(batch_size)).center(80))
 	print(('Hessian batch size = '+str(hess_batch_size)).center(80))
-    print(('Hessian low rank = '+str(args.sfn_lr)).center(80))
+	print(('Hessian low rank = '+str(args.sfn_lr)).center(80))
 	optimizer = LowRankSaddleFreeNewton(problem,regularization,sess)
 	optimizer.parameters['globalization'] = 'line_search'
 	optimizer.parameters['hessian_low_rank'] = args.sfn_lr
@@ -129,7 +129,7 @@ elif args.optimizer == 'lrsfn':
 elif args.optimizer == 'sgd':
 	batch_size = int(args.batch_ratio*training_data_size)
 	data = Data(raw_data,training_data_size,\
-        batch_size,hessian_batch_size = hess_batch_size,test_data_size = testing_data_size)
+		batch_size,hessian_batch_size = hess_batch_size,test_data_size = testing_data_size)
 	print(('Using stochastic gradient descent optimizer, with '+str(100*args.batch_ratio)+'% mini-batches').center(80))
 	print(('Batch size = '+str(batch_size)).center(80))
 	optimizer = GradientDescent(problem,regularization,sess)
@@ -143,8 +143,8 @@ optimizer.parameters['alpha'] = alpha
 print(80*'#')
 # First print
 print('{0:8} {1:11} {2:11} {3:11} {4:11}'.format(\
-                        'Sweeps'.center(8),'Loss'.center(8),'||g||'.center(8),\
-                                            'Loss_test'.center(8), 'alpha'.center(8)))
+						'Sweeps'.center(8),'Loss'.center(8),'||g||'.center(8),\
+											'Loss_test'.center(8), 'alpha'.center(8)))
 
 
 x_test, y_test = next(iter(data.test))
@@ -156,47 +156,47 @@ x_batch,y_batch = next(train_data)
 sweeps = 0
 
 for i, (data_g,data_H) in enumerate(zip(data.train,data.hess_train)):
-    x_batch,y_batch = data_g
-    x_hess, y_hess = data_H
-    feed_dict = {problem.x: x_batch}
-    hess_dict = {problem.x: x_hess}
-    if i%1==0:
-        norm_g = sess.run(problem.norm_g,feed_dict)
-        logger['||g||'][i] = norm_g
-        loss_train = sess.run(problem.loss, feed_dict)
-        logger['loss'][i] = loss_train
-        sweeps = np.dot(batch_factor,optimizer.sweeps)
-        logger['sweeps'][i] = sweeps
-        loss_test = sess.run(problem.loss,test_dict)
-        loss_test = sess.run(problem.loss,test_dict)
-        logger['loss_test'][i] = loss_test
-        try:
-            print(' {0:^8.2f} {1:1.4e} {2:1.4e} {3:1.4e} {4:1.4e}'.format(\
-                sweeps, loss_train,norm_g,loss_test,optimizer.alpha))
-        except:
-            print(' {0:^8.2f} {1:1.4e} {2:1.4e} {3:1.4e} {4:11}'.format(\
-                sweeps, loss_train,norm_g,loss_test,optimizer.alpha))
-        try:
-            sess.run(optimizer.minimize(feed_dict,hessian_feed_dict=hess_dict))
-        except:
-            sess.run(optimizer.minimize(feed_dict))
-        if args.record_spectrum:
-            k = 100
-            d,_ = low_rank_hessian(optimizer,hess_dict,k)
-            logger['lambdases'][i] = d
-            d_test,_ = low_rank_hessian(optimizer,test_dict,k)
-            logger['lambdases_test'][i] = d_test
-    if sweeps > max_sweeps:
-        break
+	x_batch,y_batch = data_g
+	x_hess, y_hess = data_H
+	feed_dict = {problem.x: x_batch}
+	hess_dict = {problem.x: x_hess}
+	if i%1==0:
+		norm_g = sess.run(problem.norm_g,feed_dict)
+		logger['||g||'][i] = norm_g
+		loss_train = sess.run(problem.loss, feed_dict)
+		logger['loss'][i] = loss_train
+		sweeps = np.dot(batch_factor,optimizer.sweeps)
+		logger['sweeps'][i] = sweeps
+		loss_test = sess.run(problem.loss,test_dict)
+		loss_test = sess.run(problem.loss,test_dict)
+		logger['loss_test'][i] = loss_test
+		try:
+			print(' {0:^8.2f} {1:1.4e} {2:1.4e} {3:1.4e} {4:1.4e}'.format(\
+				sweeps, loss_train,norm_g,loss_test,optimizer.alpha))
+		except:
+			print(' {0:^8.2f} {1:1.4e} {2:1.4e} {3:1.4e} {4:11}'.format(\
+				sweeps, loss_train,norm_g,loss_test,optimizer.alpha))
+		try:
+			sess.run(optimizer.minimize(feed_dict,hessian_feed_dict=hess_dict))
+		except:
+			sess.run(optimizer.minimize(feed_dict))
+		if args.record_spectrum:
+			k = 100
+			d,_ = low_rank_hessian(optimizer,hess_dict,k)
+			logger['lambdases'][i] = d
+			d_test,_ = low_rank_hessian(optimizer,test_dict,k)
+			logger['lambdases_test'][i] = d_test
+	if sweeps > max_sweeps:
+		break
 
 outname =  str(data_func.__name__)+\
-    str(optimizer.__class__.__name__)+str(alpha)+'_'+str(training_data_size)+\
-    			'_'+str(batch_factor[-1])+'_burn'+str(args.weight_burn_in)
+	str(optimizer.__class__.__name__)+str(alpha)+'_'+str(training_data_size)+\
+				'_'+str(batch_factor[-1])+'_burn'+str(args.weight_burn_in)
 try:
-    os.makedirs('results/')
+	os.makedirs('results/')
 except:
-    pass
+	pass
 with open('results/'+ outname +'.pkl', 'wb+') as f:
-    pickle.dump(logger, f, pickle.HIGHEST_PROTOCOL)
+	pickle.dump(logger, f, pickle.HIGHEST_PROTOCOL)
 
 
