ee6eb2d8f8b5a4e6d3d5322a68cfa381f66fb58a
Tom O'Leary-Roseberry <tom.olearyroseberry@utexas.edu>
2019-05-15 21:36:22 -0500

    adding gnu license

--- hessianlearn/data/cifar.py
+++ hessianlearn/data/cifar.py
@@ -1,3 +1,19 @@
+# This file is part of the hessianlearn package
+#
+# hessianlearn is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or any later version.
+#
+# hessianlearn is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with stein variational inference methods class project.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author: Tom O'Leary-Roseberry
+# Contact: tom.olearyroseberry@utexas.edu
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
