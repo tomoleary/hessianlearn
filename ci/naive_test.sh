a158c663a9e2cf2a8082d334b398ae7fa885887d
Tom O'Leary-Roseberry <tom.olearyroseberry@utexas.edu>
2020-01-27 20:24:37 -0600

    typo

--- ci/naive_test.sh
+++ ci/naive_test.sh
@@ -2,7 +2,7 @@
 
 optimizers = ['incg']
 for optimizer in optimizers:
-	python applications/mnist/mnist_run.py -max_sweeps 1 -optimizer 'incg'
+	python applications/mnist/mnist_run.py -max_sweeps 1 -optimizer optimizer
 
 
 
