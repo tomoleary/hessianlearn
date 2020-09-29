453db1d559023a48907f09fd8bee6b870e660f96
Tom O'Leary-Roseberry <tom.olearyroseberry@gmail.com>
2020-09-29 17:22:16 -0500

    removing old comment

--- applications/mnist/mnist_vae.py
+++ applications/mnist/mnist_vae.py
@@ -117,7 +117,6 @@ vae = tf.keras.models.Model(inputs, outputs, name='vae_mlp')
 ################################################################################
 # Instantiate the problem, regularization.
 
-# problem = AutoencoderProblem(vae,inputs = inputs,dtype=tf.float32)
 problem = VariationalAutoencoderProblem(vae,z_mean,z_log_var,dtype=tf.float32)
 
 settings['tikhonov_gamma'] = 1e-2
