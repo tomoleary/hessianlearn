checkpointing with gans, what is left to do: hessian action needs to be fixed and evaluation of loss / printing during training

hessianlearn/model/model.py
@@ -104,16 +104,16 @@ class HessianlearnModel(ABC):
 			print(('Size of configuration space:  '+str(self.problem.dimension)).center(80))
 			print(('Size of training data: '+str(self.data.train_data_size)).center(80))
 			# Approximate data needed is d_W / output
-			if len(self.problem.y_prediction.shape) >2:
-				output_dimension = 1.
-				for shape in self.problem.y_prediction.shape[1:]:
-					output_dimension *= shape.value
-				# print('Shape = ',self.problem.y_prediction.shape[1:].value)
-				# output_dimension = None
-			else:
-				output_dimension = float(self.problem.y_prediction.shape[-1].value)
+			# if len(self.problem.y_prediction.shape) >2:
+			# 	output_dimension = 1.
+			# 	for shape in self.problem.y_prediction.shape[1:]:
+			# 		output_dimension *= shape.value
+			# 	# print('Shape = ',self.problem.y_prediction.shape[1:].value)
+			# 	# output_dimension = None
+			# else:
+			# 	output_dimension = float(self.problem.y_prediction.shape[-1].value)
 			print(('Approximate data cardinality needed: '\
-				+str(int(float(self.problem.dimension)/output_dimension	))).center(80))
+				+str(int(float(self.problem.dimension)/self.problem.output_dimension	))).center(80))
 			print(80*'#')
 
 		# self._sess = None
@@ -329,8 +329,14 @@ class HessianlearnModel(ABC):
 			x_test, y_test = next(iter(self.data.test))
 			if self.problem.is_autoencoder:
 				test_dict = {self.problem.x: x_test}
+			elif self.problem.is_gan:
+				random_state_gan = np.random.RandomState(seed = 0)
+				noise = random_state_gan.normal(size = (self.data.batch_size, self.problem.noise_dimension))
+				test_dict = {self.problem.x: x_test,self.problem.noise : noise}
 			else:
 				test_dict = {self.problem.x: x_test,self.problem.y_true: y_test}
+
+
 
 			# Iteration Loop
 			max_sweeps = self.settings['max_sweeps']
@@ -348,6 +354,11 @@ class HessianlearnModel(ABC):
 				if self.problem.is_autoencoder:
 					train_dict = {self.problem.x: x_batch}
 					hess_dict = {self.problem.x: x_hess}
+				elif self.problem.is_gan:
+					noise = random_state_gan.normal(size = (self.data.batch_size, self.problem.noise_dimension))
+					train_dict = {self.problem.x: x_batch,self.problem.noise : noise}
+					noise_hess = random_state_gan.normal(size = (self.data.hessian_batch_size, self.problem.noise_dimension))
+					hess_dict = {self.problem.x: x_hess, self.problem.noise: noise_hess}
 				else:
 					train_dict = {self.problem.x: x_batch, self.problem.y_true: y_batch}
 					hess_dict = {self.problem.x: x_hess, self.problem.y_true: y_hess}
@@ -368,22 +379,30 @@ class HessianlearnModel(ABC):
 				if hasattr(self.problem,'accuracy'):
 					loss_test,	accuracy_test = sess.run([self.problem.loss,self.problem.accuracy],test_dict)
 					self._logger['accuracy_test'][iteration] = accuracy_test
+					max_test_acc = max(max_test_acc,accuracy_test)
+					self._logger['max_accuracy_test'][iteration] = max_test_acc
 				else:
 					loss_test = sess.run(self.problem.loss,test_dict)
 				self._logger['loss_test'][iteration] = loss_test
 				min_test_loss = min(min_test_loss,loss_test)
-				max_test_acc = max(max_test_acc,accuracy_test)
-				self._logger['max_accuracy_test'][iteration] = max_test_acc
+
+				
 				self._logger['alpha'][iteration] = self.optimizer.alpha
 				if self.settings['optimizer'] == 'lrsfn':
 					self._logger['hessian_low_rank'][iteration] = self.optimizer.rank
 
-				if accuracy_test == max_test_acc:
+				if hasattr(self.problem,'accuracy') and accuracy_test == max_test_acc:
 					self._best_weights = sess.run(self.problem._w)
 					if len(self._logger['best_weight']) > 2:
 						self._logger['best_weight'].pop(0)
 					acc_weight_tuple = (accuracy_test,accuracy_train,sess.run(self.problem._flat_w))
 					self._logger['best_weight'].append(acc_weight_tuple) 
+				elif loss_test == min_test_loss:
+					self._best_weights = sess.run(self.problem._w)
+					if len(self._logger['best_weight']) > 2:
+						self._logger['best_weight'].pop(0)
+					loss_weight_tuple = (loss_test,loss_train,sess.run(self.problem._flat_w))
+					self._logger['best_weight'].append(loss_weight_tuple) 
 
 				sweeps = np.dot(self.data.batch_factor,self.optimizer.sweeps)
 				if self.settings['verbose'] and iteration % 1 == 0:

hessianlearn/problem/problem.py
@@ -67,6 +67,7 @@ class Problem(ABC):
 		"""
 		# Boolean to indicate if only input data should be passed into loss function
 		self._is_autoencoder = False
+		self._is_gan = False
 		# Data type
 		self._dtype = dtype
 
@@ -143,10 +144,22 @@ class Problem(ABC):
 	def is_autoencoder(self):
 		return self._is_autoencoder		
 
+	@property
+	def is_gan(self):
+		return self._is_gan
+	
+
+	@property
+	def output_dimension(self):
+		return self._output_dimension
+	
+
 	def _initialize_network(self,NeuralNetwork):
 		"""
 		This method defines the neural network model
 			-NeuralNetwork: the neural network as a tf.keras.model.Model
+
+		Must set member variable self._output_shape
 		"""
 		self._NN = NeuralNetwork
 		self.x = self.NN.inputs[0]
@@ -159,6 +172,14 @@ class Problem(ABC):
 
 		output_shape = self.NN.output_shape
 		self.y_true = tf.placeholder(self.dtype, output_shape,name='output_placeholder')
+
+
+		if len(self.y_prediction.shape) > 2:
+			self._output_shape =  1.
+			for shape in self.y_prediction.shape[1:]:
+				self._output_dimension *= shape.value
+		else:
+			self._output_dimension = self.y_prediction.shape[-1].value
 
 	def _initialize_loss(self):
 		"""
@@ -565,6 +586,8 @@ class GenerativeAdversarialNetworkProblem(Problem):
 
 		super(GenerativeAdversarialNetworkProblem,self).__init__([generator,discriminator],dtype = dtype)
 
+		self._is_gan = True
+
 	@property
 	def loss_type(self):
 		return self._loss_type
@@ -580,6 +603,11 @@ class GenerativeAdversarialNetworkProblem(Problem):
 	@property
 	def noise(self):
 		return self._noise
+
+	@property
+	def noise_dimension(self):
+		return self._noise_dimension
+	
 	
 	@property
 	def generated_images(self):
@@ -619,18 +647,29 @@ class GenerativeAdversarialNetworkProblem(Problem):
 		"""
 		This method defines the neural network model
 			-NeuralNetworks: list of [generator,discriminator] as tf.keras.model.Model 
+
+		Must set member variable self._output_shape
 		"""
 		self._generator, self._discriminator = NeuralNetworks
 
 		self._noise = self.generator.inputs[0]
 
+		self._noise_dimension = self._noise.shape[-1].value
+
 		self._generated_images = self.generator(self._noise)
 
-		self._input_images = self.discriminator.inputs[0]
+		self.x = self.discriminator.inputs[0]
 
-		self._real_predictions = self.discriminator(self._input_images)
+		self._real_prediction = self.discriminator(self.x)
 
-		self._fake_predictions = self.discriminator(self._generated_images)
+		self._fake_prediction = self.discriminator(self._generated_images)
+
+		if len(self._real_prediction.shape) > 2:
+			self._output_shape =  1.
+			for shape in self._real_prediction.shape[1:]:
+				self._output_dimension *= shape.value
+		else:
+			self._output_dimension = self._real_prediction.shape[-1].value
 
 
 
@@ -642,15 +681,16 @@ class GenerativeAdversarialNetworkProblem(Problem):
 		with tf.name_scope('loss'): # 
 			if self.loss_type == 'cross_entropy':
 				self._generator_loss = tf.reduce_mean(tf.keras.losses.categorical_crossentropy(\
-							tf.ones_like(self._fake_predictions), self._fake_predictions,from_logits=True))
+							tf.ones_like(self._fake_prediction), self._fake_prediction,from_logits=True))
 				self._discriminator_loss = tf.reduce_mean(tf.keras.losses.categorical_crossentropy(\
-							tf.ones_like(self._real_predictions), self._real_predictions,from_logits=True))+\
+							tf.ones_like(self._real_prediction), self._real_prediction,from_logits=True))+\
 							 tf.reduce_mean(tf.keras.losses.categorical_crossentropy(\
-							tf.zeros_like(self._fake_predictions), self._fake_predictions,from_logits=True))
+							tf.zeros_like(self._fake_prediction), self._fake_prediction,from_logits=True))
 
 			elif self.loss_type == 'least_squares':
 				pass
 
+			self._loss = self._generator_loss + self._discriminator_loss
 	def _initialize_derivatives(self):
 		"""
 		This method defines derivative quantities for the loss function